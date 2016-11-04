7081b3a37e2e1396b706db3b72cfd63f367eec2a
jordi <jordi@DESKTOP-EHFJKDK>
2016-11-04 20:24:54 +0100

    Successful split of player input

--- zork/zork/main.cpp
+++ zork/zork/main.cpp
@@ -4,36 +4,37 @@
 
 using namespace std;
 
-void GetArguments(const string& line, vector<string>& args)
+void GetTokens(const string& line, vector<string>& args)
 {
-	cout << line << endl;
-	
+	const char* line_c = line.c_str();
+	const int length = line.length();
+	int i = 0;
 
+	do {
+		const int start = i;
+		while(line_c[i] != ' ' && i < length) {
+			++i;
+		}
+		args.push_back(string(line_c, start, i-start));
+		++i;
+	} while (i < length);
 }
 
 int main(int argc, char* argv[])
 {
-	string playerInput;
-	
+	string player_input;
 	vector<string> arguments;
 
 	cout << "Welcome to Zork";
 	cout << endl << "> ";
 
-	while (getline(cin, playerInput))
+	while (getline(cin, player_input))
 	{
-		/*if (playerInput.length() != 0)
-		{
-			cout << "**" << endl;
-		}
-		else {
-			cout << "Remain silent, ok" << endl;
-		}
-		cout << endl << "> ";
-		*/
-		GetArguments(playerInput, arguments); 
-		cout << endl << "> ";
+		GetTokens(player_input, arguments); 
+		//game happens here
 
+		arguments.clear();
+		cout << endl << "> ";
 	}
 	
 	cout << "Thanks for playing!" << endl << endl;
