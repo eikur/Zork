863d95f00a300c291ecef6477d3030512bc0ab73
jordi <jordi@DESKTOP-EHFJKDK>
2016-11-09 19:58:42 +0100

    Room creation and into world class

--- zork/zork/World.cpp
+++ zork/zork/World.cpp
@@ -3,14 +3,21 @@
 
 World::World()
 {
-	//world building - still not ready
-	Room* room1 = new Room("Room1", "This is the room 1 description");
-	Room* room2 = new Room("Room2", "This is the room 2 description");
-	Room* room3 = new Room("Room3", "This is the room 3 description");
+	Room* house = new Room("House", "");
+	Room* attic = new Room("House attic", "The attic is dusty, and only a few rays of light enter through the cracks of the worn out roof.");
+	Room* field = new Room("Crossing field", "You are in the middle of a field. You see in the distance a thick forest everywhere you look, and a small wooden house nearby to the east.");
+	Room* forest1 = new Room("Forest", "You are in the middle of a forest. The density of vegetation and trees here make it difficult to orientate.");
+	Room* forest2 = new Room("Forest", "You are in the middle of a forest. The density of vegetation and trees here make it difficult to orientate. You hear a water stream in the distance");
+	Room* river = new Room("River bed", "A river flows from north to south, and you can see a spooky cave to the east. You can cross the river but there is no way of getting to the other side without swimming.");
+	Room* cave = new Room("Cave", "It's pitch black inside");
 
-	this->entities.push_back(room1);
-	this->entities.push_back(room2);
-	this->entities.push_back(room3);
+	this->entities.push_back(house);
+	this->entities.push_back(attic);
+	this->entities.push_back(field);
+	this->entities.push_back(forest1);
+	this->entities.push_back(forest2);
+	this->entities.push_back(river);
+	this->entities.push_back(cave);
 }
 
 World::~World()
